1a5ba0c5921d464ad9f46f3381d24ae7a52f768b
s.muntyanov <s.muntyanov@tinkoff.ru>
2024-03-13 14:37:14 +0500

    init

--- accounting/src/test/java/ru/tinkoff/seminars/accounting/TestAccountingApplication.java
+++ accounting/src/test/java/ru/tinkoff/seminars/accounting/TestAccountingApplication.java
@@ -4,16 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestAccountingApplication {
 
+    static PostgreSQLContainer postgres;
+
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return postgres;
+    }
+
+    @DynamicPropertySource
+    static void setDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url",
+                () -> postgres.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> postgres.getUsername());
+        registry.add("spring.datasource.password", () -> postgres.getPassword());
     }
 
     public static void main(String[] args) {
